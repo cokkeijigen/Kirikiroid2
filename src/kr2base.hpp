e44e88ea3139cc1e2ff106684aeb42f6707e0342
cokkeijigen <itsukezigen@gmail.com>
2026-06-09 04:25:10 +0800

    Update kr2base.hpp

--- src/kr2base.hpp
+++ src/kr2base.hpp
@@ -2,17 +2,22 @@
 #include <cstdint>
 #include <tjs.h>
 
-#if defined(__aarch64__)
-#define tvprva(arm64v8a, armeabiv7a) arm64v8a
-#elif defined(__arm__)
-#define tvprva(arm64v8a, armeabiv7a) armeabiv7a
-#else
-#define tvprva(arm64v8a, armeabiv7a) 0
-#endif
-#define TVPRVA tvprva
+#define TVPRVA kr2android::tvp::rva
+#define tvprva kr2android::tvp::rva
 
 namespace kr2android::tvp
 {
+    consteval auto rva(uintptr_t arm64v8a, uintptr_t armeabiv7a) -> uintptr_t
+    {
+        #if defined(__aarch64__)
+        return arm64v8a;
+        #elif defined(__arm__)
+        return armeabiv7a;
+        #else
+        return 0;
+        #endif
+    }
+
     static inline constexpr uintptr_t RVA_TVP_GRAPHIC_TYPE{ TVPRVA(0x1AD84B8, 0) };
 
     static inline constexpr uintptr_t RVA_TVP_PROJECT_DIR { TVPRVA(0x1AF01A8, 0) };
