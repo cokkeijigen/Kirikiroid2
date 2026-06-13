b93bf63e141fa6d2d7e8e68b52c2278885fdef52
cokkeijigen <itsukezigen@gmail.com>
2026-06-13 10:31:58 +0800

    Update kr2rva.hpp

--- src/kr2rva.hpp
+++ src/kr2rva.hpp
@@ -2,12 +2,9 @@
 #include <cstdint>
 #include <tjs.h>
 
-#define TVPRVA kr2android::tvp::rva::rva
-#define tvprva kr2android::tvp::rva::rva
-
 namespace kr2android::tvp::rva
 {
-    consteval auto rva(uintptr_t arm64v8a, uintptr_t armeabiv7a) -> uintptr_t
+    consteval auto TargetRVA(uintptr_t arm64v8a, uintptr_t armeabiv7a) -> uintptr_t
     {
         #if defined(__aarch64__)
         return arm64v8a;
@@ -18,48 +15,64 @@ namespace kr2android::tvp::rva
         #endif
     }
 
-    struct TJSString
+    namespace TJSString
     {
-        static inline constexpr uintptr_t            ProjectDir{ TVPRVA(0x1AF01A8, 0) };
+        static inline constexpr uintptr_t ProjectDir{ TargetRVA(0x1AF01A8, 0) };
     };
 
-    struct GraphicType
+    namespace Graphic
     {
-        static inline constexpr uintptr_t               Global{ TVPRVA(0x1AD84B8, 0) };
+        static inline constexpr uintptr_t       GraphicType{ TargetRVA(0x1AD84B8, 0) };
+        static inline constexpr uintptr_t ClearGraphicCache{ TargetRVA(0x07EC31C, 0) };
+
+        namespace NativeBaseBitmap
+        {
+            static inline constexpr uintptr_t     GetWidth{ TargetRVA(0x0A74E90, 0) };
+            static inline constexpr uintptr_t    GetHeight{ TargetRVA(0x0A75020, 0) };
+            static inline constexpr uintptr_t  GetScanLine{ TargetRVA(0x0A75330, 0) };
+        }
     };
 
-    struct StorageMediaManager
+    namespace StorageMediaManager
     {
-        static inline constexpr uintptr_t                   Global{ TVPRVA(0x1AE0008, 0) };
-        static inline constexpr uintptr_t     NormalizeStorageName{ TVPRVA(0x08E93D0, 0) };
-        static inline constexpr uintptr_t      SetCurrentDirectory{ TVPRVA(0x08EA130, 0) };
-        static inline constexpr uintptr_t GetLocallyAccessibleName{ TVPRVA(0x08EA5E0, 0) };
+        static inline constexpr uintptr_t             MediaManager{ TargetRVA(0x1AE0008, 0) };
+        static inline constexpr uintptr_t     NormalizeStorageName{ TargetRVA(0x08E93D0, 0) };
+        static inline constexpr uintptr_t      SetCurrentDirectory{ TargetRVA(0x08EA130, 0) };
+        static inline constexpr uintptr_t GetLocallyAccessibleName{ TargetRVA(0x08EA5E0, 0) };
     };
 
-    static inline constexpr uintptr_t                          AddAutoPath{ TVPRVA(0x08EB894, 0) };
-    static inline constexpr uintptr_t                       RemoveAutoPath{ TVPRVA(0x08EBA70, 0) };
-    static inline constexpr uintptr_t                        GetPlacedPath{ TVPRVA(0x08EBC80, 0) };
+    namespace XP3Archive
+    {
+        static inline constexpr uintptr_t         ExtractionFilter{ TargetRVA(0x1AF0240, 0) };
+        static inline constexpr uintptr_t            ContentFilter{ TargetRVA(0x1AF0248, 0) };
+    };
 
-    static inline constexpr uintptr_t            IsExistentStorageNoSearch{ TVPRVA(0x08EB4C0, 0) };
-    static inline constexpr uintptr_t IsExistentStorageNoSearchNoNormalize{ TVPRVA(0x08EAF88, 0) };
-    static inline constexpr uintptr_t                  SetCurrentDirectory{ TVPRVA(0x08EA6F8, 0) };
-    static inline constexpr uintptr_t                   ClearStorageCaches{ TVPRVA(0x08FD444, 0) };
+    static inline constexpr uintptr_t                          AddAutoPath{ TargetRVA(0x08EB894, 0) };
+    static inline constexpr uintptr_t                       RemoveAutoPath{ TargetRVA(0x08EBA70, 0) };
+    static inline constexpr uintptr_t                        GetPlacedPath{ TargetRVA(0x08EBC80, 0) };
 
-    static inline constexpr uintptr_t              CreateTextStreamForRead{ TVPRVA(0x8F6438, 0) };
-    static inline constexpr uintptr_t             CreateTextStreamForWrite{ TVPRVA(0x8F6B14, 0) };
-    static inline constexpr uintptr_t            CreateBinaryStreamForRead{ TVPRVA(0x8DBE98, 0) };
-    static inline constexpr uintptr_t           CreateBinaryStreamForWrite{ TVPRVA(0x8DBFB4, 0) };
+    static inline constexpr uintptr_t            IsExistentStorageNoSearch{ TargetRVA(0x08EB4C0, 0) };
+    static inline constexpr uintptr_t IsExistentStorageNoSearchNoNormalize{ TargetRVA(0x08EAF88, 0) };
+    static inline constexpr uintptr_t                  SetCurrentDirectory{ TargetRVA(0x08EA6F8, 0) };
+    static inline constexpr uintptr_t                   ClearStorageCaches{ TargetRVA(0x08FD444, 0) };
 
-    static inline constexpr uintptr_t               GetDefaultReadEncoding{ TVPRVA(0x08F72C4, 0) };
-    static inline constexpr uintptr_t               SetDefaultReadEncoding{ TVPRVA(0x08F6EDC, 0) };
+    static inline constexpr uintptr_t              CreateTextStreamForRead{ TargetRVA(0x8F6438, 0) };
+    static inline constexpr uintptr_t             CreateTextStreamForWrite{ TargetRVA(0x8F6B14, 0) };
+    static inline constexpr uintptr_t            CreateBinaryStreamForRead{ TargetRVA(0x8DBE98, 0) };
+    static inline constexpr uintptr_t           CreateBinaryStreamForWrite{ TargetRVA(0x8DBFB4, 0) };
 
-    static inline constexpr uintptr_t                       GetCommandLine{ TVPRVA(0x090DE30, 0) };
-    static inline constexpr uintptr_t                       SetCommandLine{ TVPRVA(0x090FCA0, 0) };
-    static inline constexpr uintptr_t                         GetTickCount{ TVPRVA(0x0A2A4DC, 0) };
+    static inline constexpr uintptr_t               GetDefaultReadEncoding{ TargetRVA(0x08F72C4, 0) };
+    static inline constexpr uintptr_t               SetDefaultReadEncoding{ TargetRVA(0x08F6EDC, 0) };
 
+    static inline constexpr uintptr_t                       GetCommandLine{ TargetRVA(0x090DE30, 0) };
+    static inline constexpr uintptr_t                       SetCommandLine{ TargetRVA(0x090FCA0, 0) };
+    static inline constexpr uintptr_t                         GetTickCount{ TargetRVA(0x0A2A4DC, 0) };
 }
 
 namespace TVP::RVA
 {
     using namespace kr2android::tvp::rva;
 };
+
+#define tvprva(arm64v8a, armeabiv7a) TVP::RVA::TargetRVA(arm64v8a, armeabiv7a)
+#define TVPRVA(arm64v8a, armeabiv7a) TVP::RVA::TargetRVA(arm64v8a, armeabiv7a)
