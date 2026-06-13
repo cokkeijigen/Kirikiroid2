979801051737bacadd182be614c4048eb0d04747
cokkeijigen <itsukezigen@gmail.com>
2026-06-13 10:32:07 +0800

    Update k2atvp.hpp

--- src/k2atvp.hpp
+++ src/k2atvp.hpp
@@ -10,47 +10,90 @@ namespace kr2android::tvp
         static auto get() noexcept -> const TJS::ttstr*;
     };
 
-    struct graphic_type
+    namespace storage
     {
-        static auto get() noexcept -> graphic::GraphicType*;
-    };
+        struct media_manager
+        {
+            static auto get() noexcept -> storage::StorageMediaManager*;
+        };
 
-    struct storage_media_manager
+        extern auto   register_media(storage::iStorageMedia* media) noexcept -> bool;
+        extern auto unregister_media(storage::iStorageMedia* media) noexcept -> bool;
+
+        extern auto   clear_caches() noexcept -> bool;
+        extern auto normalize_name(const ttstr& name) noexcept -> std::optional<ttstr>;
+
+        extern auto              is_existent_no_search(const ttstr& name) noexcept -> std::optional<bool>;
+        extern auto is_existent_no_search_no_normalize(const ttstr& name) noexcept -> std::optional<bool>;
+
+        extern auto  set_current_directory(const ttstr& directory) noexcept -> bool;
+
+        extern auto    add_auto_path(const ttstr& path) noexcept -> bool;
+        extern auto remove_auto_path(const ttstr& path) noexcept -> bool;
+        extern auto  get_placed_path(const ttstr& path) noexcept -> std::optional<ttstr>;
+        extern auto      is_existent(const ttstr& path) noexcept -> std::optional<bool>;
+
+        extern auto get_local_name(ttstr& name) noexcept -> std::optional<ttstr>;
+
+        extern auto  extract_ext(const ttstr& name) noexcept -> ttstr;
+        extern auto extract_name(const ttstr& name) noexcept -> ttstr;
+        extern auto extract_path(const ttstr& name) noexcept -> ttstr;
+        extern auto     chop_ext(const ttstr& name) noexcept -> ttstr;
+    }
+
+    namespace xp3
     {
-        static auto get() noexcept -> storage::StorageMediaManager*;
-    };
+        #pragma pack(push, 4)
+        struct ExtractionFilterInfo
+        {
+            const tjs_uint SizeOfSelf{ sizeof(ExtractionFilterInfo) }; // structure size of ExtractionFilterInfo itself
+            const tjs_uint64   Offset; // offset of the buffer data in uncompressed stream position
+            void*              Buffer; // target data buffer
+            const tjs_uint BufferSize; // buffer size in bytes pointed by "Buffer"
+            const tjs_uint32 FileHash; // hash value of the file (since inteface v2)
+            const ttstr&     FileName;
+        };
+        #pragma pack(pop)
 
-    extern auto       add_auto_path(const ttstr& path) noexcept -> bool;
-    extern auto    remove_auto_path(const ttstr& path) noexcept -> bool;
-    extern auto     get_placed_path(const ttstr& path) noexcept -> std::optional<ttstr>;
-    extern auto is_existent_storage(const ttstr& path) noexcept -> std::optional<bool>;
+        using ExtractionFilterType = void   (*)(ExtractionFilterInfo *info, tTJSVariant *ctx);
+        using    ContentFilterType = tjs_int(*)(const ttstr &filepath, const ttstr &archivename, tjs_uint64 filesize, tTJSVariant *ctx);
+        using extraction_filter_t  = ExtractionFilterType;
+        using    content_filter_t  = ContentFilterType;
 
-    extern auto get_game_path () noexcept -> std::optional<ttstr>;
-    extern auto get_app_path  () noexcept -> std::optional<ttstr>;
-    extern auto get_local_name(ttstr& name) noexcept -> std::optional<ttstr>;
+        auto    get_content_filter() noexcept -> content_filter_t;
+        auto get_extraction_filter() noexcept -> extraction_filter_t;
 
-    extern auto  extract_storage_ext(const ttstr& name) noexcept -> ttstr;
-    extern auto extract_storage_name(const ttstr& name) noexcept -> ttstr;
-    extern auto extract_storage_path(const ttstr& name) noexcept -> ttstr;
-    extern auto     chop_storage_ext(const ttstr& name) noexcept -> ttstr;
+        auto    set_content_filter(content_filter_t    filter) noexcept -> bool;
+        auto set_extraction_filter(extraction_filter_t filter) noexcept -> bool;
+    }
 
-    extern auto   register_storage_media(storage::iStorageMedia* media) noexcept -> bool;
-    extern auto unregister_storage_media(storage::iStorageMedia* media) noexcept -> bool;
+    namespace graphic
+    {
+        struct type
+        {
+            static auto get() noexcept -> graphic::GraphicType*;
+        };
+        extern auto   register_loading_handler(const graphic::HandlerType& handler) noexcept -> bool;
+        extern auto unregister_loading_handler(const graphic::HandlerType& handler) noexcept -> bool;
+        extern auto clear_cache() noexcept -> bool;
+    }
 
-    extern auto              is_existent_storage_no_search(const ttstr& name) noexcept -> std::optional<bool>;
-    extern auto is_existent_storage_no_search_no_normalize(const ttstr& name) noexcept -> std::optional<bool>;
+    namespace stream
+    {
+        extern auto    create_text_for_read(const ttstr& name, const ttstr& modestr) noexcept -> iTJSTextReadStream*;
+        extern auto   create_text_for_write(const ttstr& name, const ttstr& modestr) noexcept -> iTJSTextWriteStream*;
+        extern auto  create_binary_for_read(const ttstr& name, const ttstr& modestr) noexcept -> tTJSBinaryStream*;
+        extern auto create_binary_for_write(const ttstr& name, const ttstr& modestr) noexcept -> tTJSBinaryStream*;
+    }
 
-    extern auto   clear_storage_caches() noexcept -> bool;
-    extern auto  set_current_directory(const ttstr& directory) noexcept -> bool;
-    extern auto normalize_storage_name(const ttstr&      name) noexcept -> std::optional<ttstr>;
+    namespace scripts
+    {
+        extern auto get_text_encoding() noexcept -> const tjs_char*;
+        extern auto set_text_encoding(const ttstr& name) noexcept -> bool;
+    }
 
-    extern auto    create_text_stream_for_read(const ttstr& name, const ttstr& modestr) noexcept -> iTJSTextReadStream*;
-    extern auto   create_text_stream_for_write(const ttstr& name, const ttstr& modestr) noexcept -> iTJSTextWriteStream*;
-    extern auto  create_binary_stream_for_read(const ttstr& name, const ttstr& modestr) noexcept -> tTJSBinaryStream*;
-    extern auto create_binary_stream_for_write(const ttstr& name, const ttstr& modestr) noexcept -> tTJSBinaryStream*;
-
-    extern auto get_default_read_encoding() noexcept -> const tjs_char*;
-    extern auto set_default_read_encoding(const ttstr& name) noexcept -> bool;
+    extern auto get_game_path() noexcept -> std::optional<ttstr>;
+    extern auto get_app_path () noexcept -> std::optional<ttstr>;
 
     extern auto set_command_line(const tjs_char* name, const ttstr& value) -> bool;
     extern auto get_command_line(const tjs_char* name, tTJSVariant* value) -> std::optional<bool>;
