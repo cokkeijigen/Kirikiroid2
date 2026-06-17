1353d175b4f86676a219f9a1f6332e6f5f075134
cokkeijigen <itsukezigen@gmail.com>
2026-06-17 10:04:46 +0800

    Update k2atjs.cpp

--- src/k2atjs.cpp
+++ src/k2atjs.cpp
@@ -1,9 +1,13 @@
 #include <tjs.h>
 #include <k2atvp.hpp>
 #include <kr2android.hpp>
+#include <tjsRandomGenerator.h>
 
 namespace TJS
 {
+    static auto _TJSGetRandomBits128(void* dest) -> void { k2a::tvp::get_random_bits128(dest);  }
+    decltype(TJSGetRandomBits128) TJSGetRandomBits128 = _TJSGetRandomBits128;
+
     decltype(TJSCreateTextStreamForRead )   TJSCreateTextStreamForRead    { k2a::tvp::stream:: create_text_for_read   };
     decltype(TJSCreateTextStreamForWrite)   TJSCreateTextStreamForWrite   { k2a::tvp::stream::create_text_for_write   };
     decltype(TJSCreateBinaryStreamForRead ) TJSCreateBinaryStreamForRead  { k2a::tvp::stream::create_binary_for_read  };
