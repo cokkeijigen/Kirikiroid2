83c56ad34e98ed7a8f00635252274c89a0ea683b
cokkeijigen <itsukezigen@gmail.com>
2026-06-17 14:29:21 +0800

    Update plugin_manager.hpp

--- Kirikiroid2Patch/app/src/main/cpp/src/plugin/plugin_manager.hpp
+++ Kirikiroid2Patch/app/src/main/cpp/src/plugin/plugin_manager.hpp
@@ -20,9 +20,10 @@ namespace kr2patch
         std::filesystem::path m_plugin_path{};
         JavaVM* m_jvm;
 
-        [[gnu::noinline]] auto   _load(const std::filesystem::path& path) noexcept -> bool;
-        [[gnu::noinline]] auto _unload(const std::filesystem::path& path) noexcept -> bool;
-        [[gnu::noinline]] auto _unload(void* handle) noexcept -> void;
+        [[gnu::noinline]] auto    _load(const std::filesystem::path& path) noexcept -> bool;
+        [[gnu::noinline]] auto  _unload(const std::filesystem::path& path) noexcept -> bool;
+        [[gnu::noinline]] auto  _unload(void* handle) noexcept -> void;
+        [[gnu::noinline]] auto _is_load(const std::filesystem::path& path) noexcept -> bool;
 
     public:
         ~plugin_manager() noexcept;
@@ -34,6 +35,9 @@ namespace kr2patch
 
         auto load(const std::filesystem::path& path) noexcept -> bool;
         auto load(std::string_view             path) noexcept -> bool;
+
+        auto is_load(const std::filesystem::path& path) noexcept -> bool;
+        auto is_load(std::string_view             path) noexcept -> bool;
 
         auto is_init() noexcept -> bool;
         auto init(JavaVM* const jvm) noexcept -> bool;
