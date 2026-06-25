fca31e642d13a58a7cb2c95bafb909e6879cc67e
cokkeijigen <itsukezigen@gmail.com>
2026-06-26 02:49:01 +0800

    Update kr2android.hpp

--- src/kr2android.hpp
+++ src/kr2android.hpp
@@ -9,49 +9,123 @@ namespace kr2android
     concept is_pointer = std::is_pointer<T>::value || std::is_member_function_pointer_v<std::remove_reference_t<T>> ||
                          std::is_function_v<std::remove_pointer_t<std::remove_reference_t<T>>>;
 
-    union uniptr_t
-    {
-        void*     ptr;
-        intptr_t  intptr;
-        uintptr_t uintptr;
-
-        inline uniptr_t() noexcept : ptr{ nullptr }{}
-        inline uniptr_t(is_pointer    auto _ptr) noexcept :     ptr { *reinterpret_cast<void**>(&_ptr) } {}
-        inline uniptr_t(std::integral auto _val) noexcept : uintptr { static_cast<uintptr_t>(_val)     } {}
-        inline auto operator+(uniptr_t other) noexcept -> uniptr_t { return other.uintptr + this->uintptr; }
-    };
-
     template<is_pointer T = void*>
     union unirawptr_t
     {
         T raw;
         void* ptr;
 
-        inline unirawptr_t() noexcept : ptr{ nullptr } {}
-        inline unirawptr_t(void* _ptr) noexcept : ptr{ _ptr } {}
-        inline unirawptr_t(T _raw) noexcept requires (!std::is_same_v<T, void*>) : raw{ _raw }{ }
-        inline unirawptr_t(std::integral auto _ptr) noexcept
+        inline constexpr unirawptr_t() noexcept : ptr{ nullptr } {}
+        inline constexpr unirawptr_t(std::nullptr_t) noexcept : ptr{ nullptr }{}
+        inline constexpr unirawptr_t(void* _ptr) noexcept : ptr{ _ptr } {}
+        inline constexpr unirawptr_t(T     _raw) noexcept requires (!std::is_same_v<T, void*>) : raw{ _raw }{ }
+        inline constexpr unirawptr_t(std::integral auto _ptr) noexcept
         {
             this->ptr = reinterpret_cast<void*>(static_cast<uintptr_t>(_ptr));
         }
     };
 
-    struct kr2android
+    union uniptr_t
     {
-        void* base;
-        virtual auto query(const char* name) noexcept -> void* = 0;
+        void*     ptr;
+        intptr_t  intptr;
+        uintptr_t uintptr;
 
-        template<is_pointer T>
-        inline auto query_cast(const char* name) noexcept -> T
+        inline constexpr uniptr_t() noexcept : ptr{ nullptr }{}
+        inline constexpr uniptr_t(std::nullptr_t) noexcept : ptr{ nullptr }{}
+        inline constexpr uniptr_t(std::integral auto _val) noexcept : ptr { unirawptr_t{ _val }.ptr } {}
+        inline constexpr uniptr_t(is_pointer    auto _ptr) noexcept : ptr { unirawptr_t{ _ptr }.ptr } {}
+        inline auto operator+(uniptr_t other) noexcept -> uniptr_t { return other.uintptr + this->uintptr; }
+    };
+
+    namespace symbol_hash
+    {
+        static inline consteval auto operator""_hash(const char* str, size_t size) noexcept -> uint64_t
         {
-            unirawptr_t<T> result{ .ptr = this->query(name) };
+            constexpr uint64_t fnv1a_prime { 0x00000100000001B3 };
+            constexpr uint64_t fnv1a_offset{ 0xCBF29CE484222325 };
+            uint64_t hash{ fnv1a_offset };
+            for (size_t i{}; i < size; i++)
+            {
+                if(str[i] ==  ' ') continue;
+                if(str[i] == '\n') continue;
+                if(str[i] == '\r') continue;
+                hash ^= static_cast<uint64_t>(str[i]);
+                hash *= fnv1a_prime;
+            }
+            return hash;
+        }
+    }
+
+    class k2aplugin
+    {
+        void*    m_base{};
+        void*(*m_query)(uint64_t hash){};
+
+    public:
+
+        inline k2aplugin() noexcept{};
+        inline k2aplugin(void* base, void*(*query)(uint64_t)) noexcept: m_base{ base }, m_query{ query }{}
+        inline k2aplugin(const k2aplugin& k2a) noexcept: k2aplugin{ &k2a }{};
+        inline k2aplugin(const k2aplugin* k2a) noexcept
+        {
+            if(k2a != nullptr)
+            {
+                this->m_base  = k2a->m_base;
+                this->m_query = k2a->m_query;
+            }
+        }
+
+        inline auto is_vaild() const noexcept -> bool
+        {
+            return bool
+            {
+                static_cast<const void*>(this) != nullptr &&
+                this->m_base != nullptr && this->m_query != nullptr
+            };
+        }
+
+        template<is_pointer T = void*>
+        inline auto query(uint64_t hash) noexcept -> T
+        {
+            unirawptr_t<T> result{};
+            if(static_cast<void*>(this) != nullptr && this->m_query != nullptr)
+            {
+                result.ptr = this->m_query(hash);
+            }
             return result.raw;
+        }
+
+        inline auto base() noexcept -> uniptr_t
+        {
+            if(static_cast<void*>(this) != nullptr)
+            {
+                return this->m_base;
+            }
+            return nullptr;
         }
     };
 
-    extern auto get_base() noexcept -> uniptr_t;
-    extern auto init(const uniptr_t libbase) noexcept -> bool;
-    extern auto init() noexcept -> bool;
+    extern k2aplugin plugin;
+    inline auto plugin_init(const k2aplugin* k2a) noexcept -> bool
+    {
+        if(k2a->is_vaild())
+        {
+            plugin = k2a;
+            return true;
+        }
+        return false;
+    }
+
+    inline auto plugin_init(const k2aplugin& k2a) noexcept -> bool
+    {
+        if(k2a.is_vaild())
+        {
+            plugin = k2a;
+            return true;
+        }
+        return false;
+    }
 
     template<is_pointer T = void*>
     inline auto cast_ptr(const uintptr_t rva) noexcept -> T
@@ -59,7 +133,7 @@ namespace kr2android
         unirawptr_t<T> result{};
         if(rva != 0)
         {
-            const uniptr_t base{ get_base() };
+            const uniptr_t base{ plugin.base() };
             if(base.ptr != nullptr)
             {
                 result = base.uintptr + rva;
@@ -67,7 +141,12 @@ namespace kr2android
         }
         return result.raw;
     }
-}
 
+    template<is_pointer T = void*>
+    inline auto cast_ptr(T& val, const uintptr_t rva) noexcept -> void
+    {
+        val = cast_ptr<T>(rva);
+    }
+}
 namespace tvp = kr2android::tvp;
 namespace k2a = kr2android;
