d706def47606dd5a6a16baf64b07817693aa9573
cokkeijigen <itsukezigen@gmail.com>
2026-07-03 05:42:50 +0800

    Update funcutils.hpp

--- Kirikiroid2Patch/app/src/main/cpp/src/plugin/utils/funcutils.hpp
+++ Kirikiroid2Patch/app/src/main/cpp/src/plugin/utils/funcutils.hpp
@@ -3,37 +3,37 @@
 namespace func
 {
     template <class ...Args, class R>
-    [[nodiscard]] constexpr auto request(R(*func)(Args...)) noexcept -> R(*)(Args...)
+    [[nodiscard]] constexpr auto of(R(*func)(Args...)) noexcept -> R(*)(Args...)
     {
         return func;
     }
 
     template <class... Args, class ClassType, class R>
-    [[nodiscard]] constexpr auto request(R(ClassType::* func)(Args...) const) noexcept -> R(ClassType::*)(Args...) const
+    [[nodiscard]] constexpr auto of(R(ClassType::* func)(Args...) const) noexcept -> R(ClassType::*)(Args...) const
     {
         return func;
     }
 
     template <class ...Args, class ClassType, class R>
-    [[nodiscard]] constexpr auto request(R(ClassType::* func)(Args...)) noexcept -> R(ClassType::*)(Args...)
+    [[nodiscard]] constexpr auto of(R(ClassType::* func)(Args...)) noexcept -> R(ClassType::*)(Args...)
     {
         return func;
     }
 
     template <class ClassType, class R>
-    [[nodiscard]] constexpr auto request(R(ClassType::* func)()) noexcept -> R(ClassType::*)()
+    [[nodiscard]] constexpr auto of(R(ClassType::* func)()) noexcept -> R(ClassType::*)()
     {
         return func;
     }
 
     template <class ClassType, class R>
-    [[nodiscard]] constexpr auto request(R(ClassType::* func)() const) noexcept -> R(ClassType::*)() const
+    [[nodiscard]] constexpr auto of(R(ClassType::* func)() const) noexcept -> R(ClassType::*)() const
     {
         return func;
     }
 
     template <class R>
-    [[nodiscard]] constexpr auto request(R(*func)()) noexcept -> R(*)()
+    [[nodiscard]] constexpr auto of(R(*func)()) noexcept -> R(*)()
     {
         return func;
     }
