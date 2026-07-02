0af585beaf105742299929737ef841d2212bdd6e
cokkeijigen <itsukezigen@gmail.com>
2026-07-03 05:34:29 +0800

    Update funcutils.hpp

--- Kirikiroid2Patch/app/src/main/cpp/src/plugin/utils/funcutils.hpp
+++ Kirikiroid2Patch/app/src/main/cpp/src/plugin/utils/funcutils.hpp
@@ -37,4 +37,84 @@ namespace func
     {
         return func;
     }
+
+    template <class... Args>
+    struct args
+    {
+        template <class R>
+        [[nodiscard]] static constexpr auto ret(R(*func)(Args...)) noexcept -> R(*)(Args...)
+        {
+            return func;
+        }
+
+        template <class R>
+        [[nodiscard]] static constexpr auto ret(R(*func)(Args...) noexcept) noexcept -> R(*)(Args...) noexcept
+        {
+            return func;
+        }
+
+        template <class R, class ClassType>
+        [[nodiscard]] static constexpr auto ret(R(ClassType::*func)(Args...)) noexcept -> R(ClassType::*)(Args...)
+        {
+            return func;
+        }
+
+        template <class R, class ClassType>
+        [[nodiscard]] static constexpr auto ret(R(ClassType::*func)(Args...) noexcept) noexcept -> R(ClassType::*)(Args...) noexcept
+        {
+            return func;
+        }
+
+        template <class R, class ClassType>
+        [[nodiscard]] static constexpr auto ret(R(ClassType::*func)(Args...) const) noexcept -> R(ClassType::*)(Args...) const
+        {
+            return func;
+        }
+
+        template <class R, class ClassType>
+        [[nodiscard]] static constexpr auto ret(R(ClassType::*func)(Args...) const noexcept) noexcept -> R(ClassType::*)(Args...) const noexcept
+        {
+            return func;
+        }
+    };
+
+    template <class R>
+    struct ret
+    {
+        template <class... Args>
+        [[nodiscard]] static constexpr auto args(R(*func)(Args...)) noexcept -> R(*)(Args...)
+        {
+            return func;
+        }
+
+        template <class... Args>
+        [[nodiscard]] static constexpr auto args(R(*func)(Args...) noexcept) noexcept -> R(*)(Args...) noexcept
+        {
+            return func;
+        }
+
+        template <class... Args, class ClassType>
+        [[nodiscard]] static constexpr auto args(R(ClassType::*func)(Args...)) noexcept -> R(ClassType::*)(Args...)
+        {
+            return func;
+        }
+
+        template <class... Args, class ClassType>
+        [[nodiscard]] static constexpr auto args(R(ClassType::*func)(Args...) noexcept) noexcept -> R(ClassType::*)(Args...) noexcept
+        {
+            return func;
+        }
+
+        template <class... Args, class ClassType>
+        [[nodiscard]] static constexpr auto args(R(ClassType::*func)(Args...) const) noexcept -> R(ClassType::*)(Args...) const
+        {
+            return func;
+        }
+
+        template <class... Args, class ClassType>
+        [[nodiscard]] static constexpr auto args(R(ClassType::*func)(Args...) const noexcept) noexcept -> R(ClassType::*)(Args...) const noexcept
+        {
+            return func;
+        }
+    };
 }
