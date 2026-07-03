d48f0465f644f98bd12827c8cba6470f1524931a
cokkeijigen <itsukezigen@gmail.com>
2026-07-04 05:35:48 +0800

    Update k2atvp.hpp

--- src/k2atvp.hpp
+++ src/k2atvp.hpp
@@ -280,6 +280,17 @@ namespace kr2android::tvp
     using WaveFormat  = sound::WaveFormat;
     using wave_format = sound::wave_format;
 
+    extern auto           add_log(const ttstr& line, bool appendtoimportant = false) noexcept -> bool;
+    extern auto add_important_log(const ttstr& line) noexcept -> bool;
+
+    extern auto inputbox(ttstr& text, const ttstr& caption) noexcept -> std::optional<bool>;
+    extern auto inputbox(ttstr& text, const ttstr& caption, const ttstr& prompt) noexcept -> std::optional<bool>;
+    extern auto inputbox(ttstr& text, const ttstr& caption, const std::vector<ttstr>& vecButtons) noexcept -> std::optional<int>;
+    extern auto inputbox(ttstr& text, const ttstr& caption, const ttstr& prompt, const std::vector<ttstr>& vecButtons) noexcept -> std::optional<int>;
+
+    extern auto messagebox(const ttstr& text, const ttstr& caption) noexcept -> std::optional<bool>;
+    extern auto messagebox(const ttstr& text, const ttstr& caption, const std::vector<ttstr>& vecButtons) noexcept -> std::optional<int>;
+
     extern auto get_random_bits128(void* dest) noexcept -> bool;
 
     extern auto set_command_line(const tjs_char* name, const ttstr& value) -> bool;
