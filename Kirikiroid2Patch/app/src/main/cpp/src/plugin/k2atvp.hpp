17ce4a3da69eb94acfa1e38fdcb39427cbb9ff76
cokkeijigen <itsukezigen@gmail.com>
2026-07-10 18:20:29 +0800

    Update k2atvp.hpp

--- Kirikiroid2Patch/app/src/main/cpp/src/plugin/k2atvp.hpp
+++ Kirikiroid2Patch/app/src/main/cpp/src/plugin/k2atvp.hpp
@@ -310,6 +310,8 @@ namespace kr2android::tvp
     extern auto get_current_shift_key_state() noexcept -> std::optional<tjs_uint32>;
 
     extern auto get_tick_count() noexcept -> std::optional<tjs_uint64>;
+
+    extern auto exit() noexcept -> void;
 }
 
 #undef unitype_name
